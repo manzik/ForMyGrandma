f938748553080f3b6b3f7ee61c1c08914f22e3c8
Mohsen Yousefian <me@manzik.com>
2023-10-02 01:22:37 -0500

    Revamp logic + new feature

--- pixel.js
+++ pixel.js
@@ -1,169 +1,145 @@
-/*
- *  Table Manipulation Script
- */
-/* FUNCTIONS */
+let table;
+let mouseIsDown = false;
 
-/* Drawing Functions */
+let currentChange = [];
+let changes = [];
 
-/**
- *  access each cell in array of cells
- *  listen for mouse events, modify cell color property for 
- *  mousedown and mousemove
- * */
-let beginDraw = () => {
-    colorCollector();
+const MAX_UNDO = 6;
 
-    for(let cell of cells){
-
-        cell.addEventListener('mousedown',event => {
-            isDrawing = true;
-            modColor(cell,myColor);
-        });
-
-        cell.addEventListener('mousemove',event => {
-            if(isDrawing){
-                modColor(cell,myColor);
-            }
-        });
-
-        cell.addEventListener('mouseup',event => {
-            if(isDrawing){
-                isDrawing = false;
-            }
-        });
+window.addEventListener("load", () => {
+  table = document.getElementsByTagName("table")[0];
+  resizeTable();
+  window.addEventListener("mousedown", e => {
+    // check if it's in the table
+    let target = e.target;
+    while (target && target !== table) {
+      target = target.parentNode;
     }
-} 
-
-/* Color Mod Functions */
-
-/* set myColor to the current selected color from the dropdown menu */
-let colorCollector = () =>
-{
-    myColor = document.getElementById("color").value;
-    
-}
-
-/* change color of cell based on input */
-let modColor = (loc,clr) => {
-  console.log(clr, loc.style.backgroundColor);
-  if (clr != loc.style.backgroundColor) {
-    loc.style.backgroundColor = clr;
-  }
-}
-
-/* helper to grab current color value */
-let allColorHelper = () => {
-    //get current color
-    allColor(myColor);
-}
-
-/* change color of all cells based on input */
-let allColor = (clr) => {
-    for(let cell of cells){
-        modColor(cell,clr);
+    if (target === table) {
+      e.preventDefault();
     }
-}
-
-/* helper to grab current color value */
-let remColorHelper = () => {
-    //get current color
-    remColor(myColor);
-}
-
-/* change color of all cells based on input */
-let remColor = (clr) => {
-    for(let cell of cells){
-        if(cell.style.backgroundColor == "gainsboro"){
-            modColor(cell,clr);
+    mouseIsDown = true;
+  });
+  window.addEventListener("mouseup", () => {
+    if (currentChange.length) {
+      while (changes.length > MAX_UNDO) {
+        changes.shift();
+      }
+      changes.push(currentChange);
+      currentChange = [];
+    }
+    mouseIsDown = false;
+  });
+  window.addEventListener("mousemove", (event) => {
+    if (mouseIsDown) {
+      let target = event.target;
+      if (target.tagName === "TD") {
+        if (target.style.backgroundColor !== document.getElementById("color").value) {
+          currentChange.push({
+            element: target,
+            color: target.style.backgroundColor
+          });
+          target.style.backgroundColor = document.getElementById("color").value;
         }
+      }
     }
+  });
+});
+
+function undo() {
+  let change = changes.pop();
+  console.log(change);
+  if (change) {
+    for(let i = 0; i < change.length; i++) {
+      change[i].element.style.backgroundColor = change[i].color;
+    }
+  }
 }
 
-let initialFill = () => {
-    for(let cell of cells){
-        if(cell.style.backgroundColor == ""){
-            modColor(cell,"gainsboro");
+window.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === 'z') {
+    undo();
+  }
+});
+
+function resizeTable() {
+  let newRowsCount = parseInt(document.getElementById("table-rows").value, 10);
+  let newColumnsCount = parseInt(document.getElementById("table-columns").value, 10);
+
+  let currentRowsCount = table.rows.length;
+  let currentColumnsCount = table.rows.length ? table.rows[0].cells.length : 0;
+
+  let rowsChangeCount = newRowsCount - currentRowsCount;
+  let columnsChangeCount = newColumnsCount - currentColumnsCount;
+
+  // Adjust rows
+  for(let i = 0; i < Math.abs(rowsChangeCount); i++) {
+    if (rowsChangeCount > 0) {
+      let newRow = table.insertRow();
+      for(let j = 0; j < currentColumnsCount; j++) {
+        newRow.insertCell();
+        // set its background color to transparent
+        newRow.cells[j].style.backgroundColor = "rgb(255, 255, 255)";
+      }
+    } else if (rowsChangeCount < 0) {
+      table.deleteRow(-1);
+    }
+  }
+
+  // Adjust columns
+  for(let i = 0; i < table.rows.length; i++) {
+    for(let j = 0; j < Math.abs(columnsChangeCount); j++) {
+      if (columnsChangeCount > 0) {
+        table.rows[i].insertCell();
+        // set its background color to transparent
+        table.rows[i].cells[table.rows[i].cells.length - 1].style.backgroundColor = "rgb(255, 255, 255)";
+      } else if (columnsChangeCount < 0) {
+        table.rows[i].deleteCell(-1);
+      }
+    }
+  }
+}
+
+function saveFile() {
+  let data = {};
+  data.rows = table.rows.length;
+  data.columns = table.rows[0].cells.length;
+  data.colors = [];
+  for(let i = 0; i < table.rows.length; i++) {
+    data.colors[i] = [];
+    for(let j = 0; j < table.rows[i].cells.length; j++) {
+      data.colors[i][j] = table.rows[i].cells[j].style.backgroundColor;
+    }
+  }
+  let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+  let downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", "knit-save.poo");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
+function loadFile() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.accept = '.poo';
+  input.onchange = e => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    reader.readAsText(file,'UTF-8');
+    reader.onload = readerEvent => {
+      let content = readerEvent.target.result;
+      let data = JSON.parse(content);
+      document.getElementById("table-rows").value = data.rows;
+      document.getElementById("table-columns").value = data.columns;
+      resizeTable();
+      for(let i = 0; i < table.rows.length; i++) {
+        for(let j = 0; j < table.rows[i].cells.length; j++) {
+          table.rows[i].cells[j].style.backgroundColor = data.colors[i][j];
         }
-    }
-}
-
-/* Table Expansion and Contraction Functions */
-
-
-/* add 1 column at the end of the table */
-let addColumn = () => {
-  let table = document.getElementById("myTable");  
-  let rows = document.getElementsByTagName('tr');
-  let newRow = document.createElement('tr');      
-  
-  if(rows[0] != undefined){
-    for(let row of rows){
-      row.insertCell(0);
-      initialFill();
-    } 
-  }else {/*Edge case, add first Node */
-    newRow.appendChild(document.createElement('td'));
-    table.appendChild(newRow);
-    initialFill();
-  }
-
-}
-
-/* remove 1 column at the end of the table */
-let deleteColumn = () => {
-  let rows = Array.from(document.getElementsByTagName('tr'));  
-  if(rows[0].childElementCount > 0){
-    for(let row of rows){
-      row.deleteCell(0);
+      }
     }
   }
-
+  input.click();
 }
-
-/* add 1 row at the end of the table */
-let addRow = () => {
-  let table = document.getElementById("myTable");
-  let rows = Array.from(document.getElementsByTagName('tr'));
-  let newRow = document.createElement('tr');      
-  
-  // goes up until the length of the rows
-  if(rows[0] != undefined){
-    for(let i = 0; i < rows[0].childElementCount; i++){ 
-      newRow.appendChild(document.createElement('td'));
-      table.appendChild(newRow);     
-      initialFill();
-    }
-  }else{ // Edge case, add first Node
-    newRow.appendChild(document.createElement('td'));
-    table.appendChild(newRow);
-    initialFill();
-  }
-
-}
-
-/* delete 1 row at the end of the table */
-let deleteRow = () => {
-  let rows = Array.from(document.getElementsByTagName('tr'));  
-  let table = document.getElementById("myTable");
-  
-  if(rows[0] != undefined){
-    table.deleteRow(0);
-  }
-
-}
-
-
-/* MAIN */
-
-let initCells = false;
-let myColor; /* need to get current color from dropdown */
-let isDrawing = false;
-let i = 0;
-
-/* gather array of all cells */
-const cells = document.getElementsByTagName("TD");
-
-/* listen for mouse event */
-document.onmousemove = function(){
-    beginDraw();
-};
